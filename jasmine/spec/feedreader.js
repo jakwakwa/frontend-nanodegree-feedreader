fc071b85bb453a53a0c6b58b5f2d3274b952ec22
Jaco Kotzee <jaco@doodlesdigital.co.za>
2018-05-22 13:17:09 +0200

    last test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,12 +9,11 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function () {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
+    /* a test suite. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function () {
-        /* This is our first test - it tests to make sure that the
+        /* tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
          * the rest of this project. What happens when you change
@@ -27,7 +26,7 @@ $(function () {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -39,7 +38,7 @@ $(function () {
             }
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -55,7 +54,7 @@ $(function () {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function () {
-        /* TODO: Write a test that ensures the menu element is
+        /* a test that ensures the menu element is
         * hidden by default. You'll have to analyze the HTML and
         * the CSS to determine how we're performing the
         * hiding/showing of the menu element.
@@ -64,7 +63,7 @@ $(function () {
             var bodyDiv = $('body');
             expect(bodyDiv.hasClass('menu-hidden')).toBe(true);
         });
-        /* TODO: Write a test that ensures the menu changes
+        /* a test that ensures the menu changes
         * visibility when the menu icon is clicked. This test
         * should have two expectations: does the menu display when
         * clicked and does it hide when clicked again.
@@ -81,9 +80,9 @@ $(function () {
         })
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* a new test suite named "Initial Entries" */
     describe('Initial Entries', function () {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* a test that ensures when the loadFeed
         * function is called and completes its work, there is at least
         * a single .entry element within the .feed container.
         * Remember, loadFeed() is asynchronous so this test will require
@@ -100,10 +99,28 @@ $(function () {
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function () {
+        /* a test that ensures when a new feed is loaded
+        * by the loadFeed function that the content actually changes.
+        * Remember, loadFeed() is asynchronous.
+        */
+        var oldFeed;
+        var newFeed;
 
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
+        beforeEach(function (done) {
+            $('.feed').empty();
+            loadFeed(0, function () {
+                legacyFeed = $('.feed').html();
+                loadFeed(1, done);
+            });
+        });
+
+        it('content changes when loadFeed has run', function () {
+            newFeed = $('.feed').html();
+            expect(legacyFeed).not.toBe(newFeed);
+        });
+
+    });
+
 }());
